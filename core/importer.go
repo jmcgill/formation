a90d5541cfec6a2794305b83696e7538ebe61349
Jimmy <jimmy@usebutton.com>
2017-12-29 18:31:59 -0500

    Add PatchyImporter interface definition, to allow importing of EC2 resources cleanly

--- core/importer.go
+++ core/importer.go
@@ -1,5 +1,7 @@
 package core
 
+import "github.com/hashicorp/terraform/terraform"
+
 type Instance struct {
 	ID   string
 	Name string
@@ -8,4 +10,13 @@ type Instance struct {
 type Importer interface {
 	Describe(meta interface{}) ([]*Instance, error)
 	Links() map[string]string
+}
+
+// This interface should only be implemented for resources where we need to hack around bugs or limitations in
+// Terraform
+type PatchyImporter interface {
+	Describe(meta interface{}) ([]*Instance, error)
+	Links() map[string]string
+	Import(*terraform.InstanceInfo, interface{}) ([]*terraform.InstanceState, bool, error)
+	Clean(*terraform.InstanceState, interface{}) (*terraform.InstanceState)
 }
