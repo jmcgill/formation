b51b909747a871a1faae7947be255ff0e72bf3f0
James McGill <jimmy@usebutton.com>
2018-01-30 10:06:20 -0500

    Add importer for aws_load_balancer_policy

--- aws/aws_load_balancer_policy.go
+++ aws/aws_load_balancer_policy.go
@@ -2,7 +2,9 @@ package aws
 
 import (
 	"github.com/jmcgill/formation/core"
-	//"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws"
+	"strings"
+	"github.com/aws/aws-sdk-go/service/elb"
 )
 
 type AwsLoadBalancerPolicyImporter struct {
@@ -10,28 +12,56 @@ type AwsLoadBalancerPolicyImporter struct {
 
 // Lists all resources of this type
 func (*AwsLoadBalancerPolicyImporter) Describe(meta interface{}) ([]*core.Instance, error) {
-	return nil, nil
-	//svc :=  meta.(*AWSClient).elbconn
+	svc :=  meta.(*AWSClient).elbconn
 
-	// Add code to list resources here
-	//result, err := svc.ListBuckets(nil)
-	//if err != nil {
-	//  return nil, err
-	//}
+	elbs := make([]*elb.LoadBalancerDescription, 0)
+	err := svc.DescribeLoadBalancersPages(nil, func(o *elb.DescribeLoadBalancersOutput, lastPage bool) bool {
+		elbs = append(elbs, o.LoadBalancerDescriptions...)
+		return true // continue paging
+	})
+	if err != nil {
+		return nil, err
+	}
 
-	//existingInstances := ... // e.g. result.Buckets
-	//instances := make([]*core.Instance, len(existingInstances))
-	//for i, existingInstance := range existingInstances {
-	//	instances[i] = &core.Instance{
-	//		Name: strings.Replace(aws.StringValue(existingInstance.Name), "-", "_", -1),
-	//		ID:   aws.StringValue(existingInstance.Name),
-	//	}
-	//}
+	instances := make([]*core.Instance, 0)
+	for _, instance := range elbs {
+		input := &elb.DescribeLoadBalancerPoliciesInput{
+			LoadBalancerName: instance.LoadBalancerName,
+		}
+		result, err := svc.DescribeLoadBalancerPolicies(input)
+		if err != nil {
+			return nil, err
+		}
+		for _, policy := range result.PolicyDescriptions {
+			id := aws.StringValue(instance.LoadBalancerName) + ":" + aws.StringValue(policy.PolicyName)
+			i := &core.Instance{
+				Name: core.Format(id),
+				ID:   id,
+			}
+			instances = append(instances, i)
+		}
+	}
 
-	// return instances, nil
+	return instances, nil
+}
+
+func (*AwsLoadBalancerPolicyImporter) Import(in *core.Instance, meta interface{}) ([]*terraform.InstanceState, bool, error) {
+	state := &terraform.InstanceState{
+		ID: in.ID,
+	}
+
+	return []*terraform.InstanceState{
+		state,
+	}, false, nil
+}
+
+func (*AwsLoadBalancerPolicyImporter) Clean(in *terraform.InstanceState, meta interface{}) *terraform.InstanceState {
+	return in
 }
 
 // Describes which other resources this resource can reference
 func (*AwsLoadBalancerPolicyImporter) Links() map[string]string {
-	return map[string]string{}
+	return map[string]string{
+		"load_balancer": "aws_elb.name",
+	}
 }
